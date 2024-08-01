1409dc93fd1a9a6a35bad35bcace4baff6ab5797
Sofya Tarnalitskaya <tarnalitska@gmail.com>
2024-08-01 15:14:55 +0200

    Cards array with ForEach

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Sofya Tarnalitskaya on 01/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let emojis = ["👻", "🎃", "🕷️", "😈"]
+    
+    var body: some View {
+        HStack {
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sofya Tarnalitskaya on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
